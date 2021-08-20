fdeba94fec0481228e652b8b9fce9644d2228cde
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-21 00:20:54 +0530

    feat:google auth basic structure

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { googleClientID, googleClientSecret } from "./config/keys";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
