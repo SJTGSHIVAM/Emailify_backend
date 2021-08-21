4121c3a52962943a0b71c89582d2be3a93448e7f
sjtgshivam <getlovebymagicalshivam@gmail.com>
2021-08-21 20:25:56 +0530

    broken: Atlas error

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 import _ from "./services/passport.js";
 import authRoutes from "./routes/authRoutes.js";
+import { monoURI } from "./config/keys.js";
+mongoose.connect(monoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
 authRoutes(app);
 const PORT = process.env.PORT || 5000;
